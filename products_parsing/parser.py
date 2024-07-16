6522c2043d8021400f2918cf530ad4c50cfa6b59
Nikolay Milenko <nikolay.milenko@gmail.com>
2024-07-16 12:49:11 +0300

    add remove duplicates from results func

--- products_parsing/parser.py
+++ products_parsing/parser.py
@@ -4,10 +4,16 @@ import time
 import json
 import re
 
+from urllib.parse import urlparse
+import http.client
+
 from typing import List, Dict
 
 from products_parsing.config import *
 from filter import contains_banned_words
+
+
+http.client._MAXHEADERS = 1000
 
 
 def get_html_content(url):
@@ -26,9 +32,18 @@ def get_elements_text(soup, css_selector):
     return [element.text.strip() for element in elements]
 
 
-def collect_products(search_url: str, headers_: Dict, params: Dict) -> List[Dict]:
-    updated_product_data = []
+def get_domain_name(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc  # Получаем доменное имя с поддоменом, если он есть
 
+    # Используем регулярное выражение для извлечения основного доменного имени
+    match = re.search(r'(?<=\.)?([a-zA-Z0-9-]+)(?=\.[a-z]{2,})', domain)
+    if match:
+        return match.group(1)
+    return None
+
+
+def collect_products(search_url: str, headers_: Dict, params: Dict, data: List[Dict]) -> List[Dict]:
     response = requests.get(search_url, headers=headers_, params=params)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
@@ -36,11 +51,13 @@ def collect_products(search_url: str, headers_: Dict, params: Dict) -> List[Dict
     for result in soup.select('.tF2Cxc'):
         title = result.select_one('.DKV0Md').text
         link = result.select_one('.yuRUbf a')['href']
-        print(title, link)
         if any([re.search(marketplace, link) for marketplace in marketplaces]):
+            print(f'Нашелся маркетплейс: {title}, {link}')
             continue
 
-        if any([re.search(link, new_product['url']) for new_product in updated_product_data]):
+        domain_name = get_domain_name(link)
+        if any([re.search(domain_name, get_domain_name(new_product['url'])) for new_product in data]):
+            print(f'Нашелся уже добавленный сайт: {domain_name}')
             continue
 
         time.sleep(4)  # Ждём перед запросом следующей страницы
@@ -65,7 +82,7 @@ def collect_products(search_url: str, headers_: Dict, params: Dict) -> List[Dict
                     'review': review_text
                 })
 
-            updated_product_data.append({
+            data.append({
                 'url': link,
                 'name': name,
                 'description': description,
@@ -75,7 +92,7 @@ def collect_products(search_url: str, headers_: Dict, params: Dict) -> List[Dict
             })
         except Exception as e:
             print(f"Error parsing {link}: {e}")
-    return updated_product_data
+    return data
 
 
 def parse(query):
@@ -96,7 +113,7 @@ def parse(query):
     product_data = []
 
     while len(product_data) < min_results_count:
-        product_data.extend(collect_products(search_url, headers, params))
+        product_data = collect_products(search_url, headers, params, product_data)
         cur_page += 1
         time.sleep(2)
 
