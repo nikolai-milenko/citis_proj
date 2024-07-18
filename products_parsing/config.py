f0d379ec4c0aff857f61964202c1fb510615d399
Nikolay Milenko <nikolay.milenko@gmail.com>
2024-07-18 23:13:00 +0300

    rewrite parser with selenium

--- products_parsing/config.py
+++ products_parsing/config.py
@@ -4,13 +4,10 @@ marketplaces = [
     'market.yandex',
     'megamarket',
     'lamoda',
-    'dns',
-    'eldorado',
-    'mvideo',
     'google',
 ]
 
-min_results_count = 5  # не рекомендуется выставлять значение больше 10
+min_websites_count = 7  # не рекомендуется выставлять значение больше 10
 
 max_results_from_website = 10  # максимальное количество товаров, полученное в итоге с одного сайта
 
@@ -22,13 +19,16 @@ max_results_from_website = 10  # максимальное количество �
 '''
 
 headers = {
-    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
     # 'Accept-Encoding': "gzip, deflate, br, zstd",
-    # 'Accept-Language': "de,en-US;q=0.7,en;q=0.3",
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
+    'Accept-Language': "de,en-US;q=0.7,en;q=0.3",
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+    'Connection': "keep-alive",
 }
 
 websites_css_selectors = {
-    'www.citilink.ru': ['a.app-catalog-9gnskf.e1259i3g0', {'name': '.easmdi50.eml1k9j0.app-catalog-lc5se5.e1gjr6xo0', 'price': '.e1j9birj0.e106ikdt0.app-catalog-8hy98m.e1gjr6xo0', 'rating': '.e8eovjk0.app-catalog-1uwfsq8.e2kybqa2', 'reviews': 'a.app-catalog-peotpw.e1mnvjgw0', 'review_rating': '.e1ys5m360.e106ikdt0.app-catalog-rx1cfc.e1gjr6xo0', 'review_comment': 'e1ys5m360.e106ikdt0.app-catalog-rx1cfc.e1gjr6xo0'}],
-
+    'www.citilink.ru': ['a.app-catalog-9gnskf.e1259i3g0', {'name': '.easmdi50.eml1k9j0.app-catalog-lc5se5.e1gjr6xo0', 'price': '.e1j9birj0.e106ikdt0.app-catalog-8hy98m.e1gjr6xo0', 'rating': '.e8eovjk0.app-catalog-1uwfsq8.e2kybqa2', 'reviews': 'a.app-catalog-peotpw.e1mnvjgw0', 'review_rating': 'div span span.e1ys5m360.e106ikdt0.app-catalog-rx1cfc.e1gjr6xo0', 'review_comment': '.app-catalog-a9fxyi.eyoh4ac0'}],
+    'www.mvideo.ru': ['a.product-title__text.product-title--clamp', {'name': 'h1.title', 'price': '.price__main-value', 'rating': '.rating-value.medium', 'reviews': '.rating-reviews.ng-star-inserted', 'review_rating': 'total-rating', 'review_comment': 'review-text__item ng-star-inserted'}],
+    'www.dns-shop.ru': ['a.catalog-product__name.ui-link.ui-link_black', {'name': 'h1.product-card-top__title', 'price': 'div.product-buy__price', 'rating': 'a.product-card-top__rating', 'reviews': 'a.product-card-top__rating', 'review_rating': 'div div div div div div.opinion-rating-slider', 'review_comment': 'div.ow-opinion__text'}],
+    'www.vseinstrumenti.ru': ['a.-dp5Dd.clamp-3.buZF02', {'name': 'h1.typography.heading.v3.-no-margin', 'price': 'p.typography.heading.v2.-no-margin', 'rating': 'div div div.BJoVux div.rating input', 'reviews': 'section.pOjh3i.TCE984 div div div a.base-button.-small.-primary.-link.-no-paddings', 'review_rating': ''}]
 }
--- products_parsing/parser.py
+++ products_parsing/parser.py
@@ -1,20 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
 import time
 import json
 import re
+from urllib.parse import urlencode, urlparse
 
-from urllib.parse import urlparse
-import http.client
-
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from typing import List, Dict, Union, Tuple
-
 from products_parsing.config import *
 from filter import contains_banned_words
 
-http.client._MAXHEADERS = 1000
-
-# параметры для поиска, впоследствии вставятся в url
 params = {
     'q': '',  # query
     'gl': 'ru',  # country to search from
@@ -22,11 +17,23 @@ params = {
     'start': 0  # элемент начиная с которого по счету отображать результаты
 }
 
+# Настройка Selenium WebDriver
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
-def get_html_content(url: str):
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()  # Вызываем исключение при ошибке HTTP
-    return response.text
+service = webdriver.chrome.service.Service('./chromedriver.exe')  # Укажите путь к вашему chromedriver
+
+
+def get_html_content_selenium(url: str):
+    driver = webdriver.Chrome(service=service, options=options)
+    url_with_params = f"{url}?{urlencode(params)}"
+    driver.get(url_with_params)
+    time.sleep(5)  # Задержка 5 секунд
+    html = driver.page_source
+    driver.quit()
+    return html
 
 
 def get_element_text(soup, css_selector) -> Union[str, None]:
@@ -41,18 +48,12 @@ def get_elements_text(soup, css_selector) -> List[Union[str, None]]:
 
 def get_domain_name(url):
     parsed_url = urlparse(url)
-    domain = parsed_url.netloc  # Получаем доменное имя с поддоменом, если он есть
-
-    # Используем регулярное выражение для извлечения основного доменного имени
-    match = re.search(r'(?<=\.)?([a-zA-Z0-9-]+)(?=\.[a-z]{2,})', domain)
-    if match:
-        return match.group(1)
-    return None
+    domain = parsed_url.netloc
+    return domain
 
 
-def collect_search_result(search_url: str) -> List[Dict]:
-    product_websites = []
-    html_text = get_html_content(search_url)
+def collect_search_result(search_url: str, product_websites) -> List[Dict]:
+    html_text = get_html_content_selenium(search_url)
     soup = BeautifulSoup(html_text, 'lxml')
 
     for result in soup.select('.tF2Cxc'):
@@ -67,16 +68,23 @@ def collect_search_result(search_url: str) -> List[Dict]:
             print(f'Нашелся уже добавленный сайт: {domain_name}')
             continue
 
-        time.sleep(4)  # Ждём перед запросом следующей страницы
-        product_websites.append({'title': title, 'link': link, 'domain_name': domain_name})
+        product_websites.append({'title': title, 'url': link, 'domain_name': domain_name})
+        print(f'Нашелся сайт который будем парсить! {domain_name}')
+        print('Ожидание перед проверкой следующей страницы 5 с.')
+        time.sleep(5)  # Ждём перед запросом следующей страницы
+        print('Конец ожидания.')
+
+    print(f'\nДля этой страницы поиска сайты собраны! Всего собрано: {len(product_websites)}')
+
     return product_websites
 
 
 def get_products_info(product_websites: List[Dict]) -> List[Dict]:
     products_data = []
     for website in product_websites:
+        print(f'\n\nПроверка страницы: {website["url"]}')
         try:
-            product_page = get_html_content(website['url'])
+            product_page = get_html_content_selenium(website['url'])
             if contains_banned_words(product_page):
                 print(f"Страница по адресу '{website['url']}' содержит запрещённые слова и будет пропущена.")
                 continue
@@ -86,16 +94,21 @@ def get_products_info(product_websites: List[Dict]) -> List[Dict]:
             if website['domain_name'] in websites_css_selectors.keys():
                 # отлично! селекторы для этой страницы известны!
                 print(f'Для сайта с доменным именем {website["domain_name"]} нашлись селекторы!')
+                time.sleep(5)
 
                 website_info = websites_css_selectors[website['domain_name']]
-
+                print(f'Информация о веб-сайте с доменным именем {website["domain_name"]}: {website_info}')
+                print(product_soup)
+                print(product_soup.select(website_info[0]))
                 # определяем, находимся ли мы уже на странице товара или еще только на странице с листингом товаров
                 if not product_soup.select(website_info[1]['price']):
+                    print(f'Находимся на странице с листингом товаров.')
                     # находимся на странице с листингом товаров
                     products_links = product_soup.select(website_info[0])
                     for product in products_links:
-                        products_data.extend(parse_product(product, website_info))
+                        products_data.extend(parse_product(f'https://{website["domain_name"]}{product["href"]}', website_info))
                 else:
+                    print('Находимся на странице конкретного товара')
                     products_data.extend(parse_product(website['url'], website_info))
             else:
                 print(f'Для сайта с доменным именем {website["domain_name"]} НЕ НАШЛОСЬ селекторов!')
@@ -107,21 +120,34 @@ def get_products_info(product_websites: List[Dict]) -> List[Dict]:
 
 
 def parse_product(product_url, website_params) -> Dict:
-    html_text = get_html_content(product_url)
+    print(f'Ищем продукт по его url: {product_url}')
+
+    html_text = get_html_content_selenium(product_url)
     soup = BeautifulSoup(html_text, 'lxml')
 
     product_name = soup.select(website_params[1]['name'])
     if len(product_name) != 1:
         product_name = ['']
+    else:
+        product_name = [product_name[0].text]
 
     product_price = soup.select(website_params[1]['price'])
     if len(product_price) != 1:
         product_price = ['']
+    else:
+        product_price = [product_price[0].text]
 
     product_rating = soup.select(website_params[1]['rating'])
     if len(product_rating) != 1:
-        product_rating = ['']
+        product_rating = [product_rating[0]]
+    elif get_domain_name(product_url) == 'www.dns-shop.ru':
+        product_rating = [product_rating[0].get('data-rating')]
+    elif get_domain_name(product_url) == 'www.vseinstrumenti.ru':
+        product_rating = [product_rating[0].get('value')]
+    else:
+        product_rating = [product_rating[0].text]
 
+    print(product_name, product_price, product_rating)
     product_data = {
         'name': product_name[0],
         'price': product_price[0],
@@ -129,30 +155,44 @@ def parse_product(product_url, website_params) -> Dict:
     }
 
     reviews_url = soup.select_one(website_params[1]['reviews'])
+    product_data['reviews'] = parse_reviews(f'https://{get_domain_name(product_url)}{reviews_url["href"]}', product_data, website_params)
 
-    product_data['reviews'] = parse_reviews(reviews_url, product_data, website_params)
+    print(f'Собрали данные о продукте: {product_data}')
 
     return product_data
 
 
 def parse_reviews(reviews_url, product_data: Dict, website_params) -> List[Dict]:
-    html_text = get_html_content(reviews_url)
+    html_text = get_html_content_selenium(reviews_url)
     soup = BeautifulSoup(html_text, 'lxml')
 
+    print("Теперь парсим отзывы для этого сайта.")
+    print(reviews_url)
     reviews = []
     for review_comment in soup.select(website_params[1]['review_comment']):
-        reviews.append({'review': review_comment})
+        reviews.append({'review': review_comment.text})
+
+    print(f'Всего отзывов: {len(reviews)}')
 
     _ = 0
     for review_rating in soup.select(website_params[1]['review_rating']):
         reviews[_]['rating'] = review_rating
         _ += 1
+        if _ == len(reviews):
+            break
 
     return reviews
 
 
 def collect_products_info(search_url: str) -> Tuple[List[Dict], List[Dict]]:
-    product_websites = collect_search_result(search_url)  # сейвим результаты поиска в google
+    product_websites = []
+    while len(product_websites) < min_websites_count:
+        print(f'Поиск на странице google: {params["start"] // 10 + 1}')
+        product_websites = collect_search_result(search_url, product_websites)  # сейвим результаты поиска в google
+        params['start'] += 10
+        time.sleep(10)
+    print(f'\nПолучили: {product_websites}')
+    print('Теперь ищем инфу о товарах')
     products_data = get_products_info(product_websites)  # достаем все ссылки на товары, а по ним и инфу о продуктах
     return products_data
 
@@ -178,12 +218,10 @@ def parse(query):
     cur_page = 0
 
     params['query'], params['start'] = query, cur_page * 10
-    products_data = []
 
-    while len(products_data) < min_results_count:
-        products_data.extend(collect_products_info(search_url))
-        cur_page += 1
-        time.sleep(2)
+    products_data = collect_products_info(search_url)
+
+    print(f'Результат перед упаковкой: {products_data}')
 
     # Сохраняем данные в JSON файл
     with open('products.json', 'w', encoding='utf-8') as f:
@@ -191,4 +229,4 @@ def parse(query):
 
 
 # Пример использования
-parse("купить машину")
+parse("купить микроволновку")
